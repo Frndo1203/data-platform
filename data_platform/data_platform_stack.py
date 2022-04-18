b5711be986fdd04c6bea66e693f3c20beadd38a2
Frndo1203 <oliveirafer06@gmail.com>
2022-04-18 17:42:39 -0300

    Changes ipv4 peer

--- data_platform/data_platform_stack.py
+++ data_platform/data_platform_stack.py
@@ -29,7 +29,7 @@ class DataPlatformStack(core.Stack):
         )
 
         self.orders_rds_sg.add_ingress_rule(
-            peer=ec2.Peer.any_ipv4(),
+            peer=ec2.Peer.ipv4("0.0.0.0/0"),
             connection=ec2.Port.tcp(5432)
         )
 
