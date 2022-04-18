f9e58db13fa495fb1c344e25820abbba4f4f36dd
Frndo1203 <oliveirafer06@gmail.com>
2022-04-18 17:45:14 -0300

    Allows only my ipv4

--- data_platform/data_platform_stack.py
+++ data_platform/data_platform_stack.py
@@ -29,7 +29,7 @@ class DataPlatformStack(core.Stack):
         )
 
         self.orders_rds_sg.add_ingress_rule(
-            peer=ec2.Peer.ipv4("0.0.0.0/0"),
+            peer=ec2.Peer.ipv4("187.18.141.94"),
             connection=ec2.Port.tcp(5432)
         )
 
