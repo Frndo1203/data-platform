911964bb870bb2602136703b8b782758f5a0cf80
Frndo1203 <oliveirafer06@gmail.com>
2022-04-24 14:38:10 -0300

    Creates DMS

--- app.py
+++ app.py
@@ -2,8 +2,11 @@
 from aws_cdk import core
 from data_platform.data_lake.stack import DataLakeStack
 from data_platform.data_platform_stack import DataPlatformStack
+from data_platform.dms.stack import DMSStack
 
 app = core.App()
 data_lake = DataLakeStack(app)
 data_platform = DataPlatformStack(app)
+dms = DMSStack(app, common_stack=DataPlatformStack, 
+                data_lake_raw_bucket=data_lake.data_lake_raw_bucket)
 app.synth()
