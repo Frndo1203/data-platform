67fa401e9188fb6226c59a5071b25abc0810c21c
Frndo1203 <oliveirafer06@gmail.com>
2022-04-24 14:43:27 -0300

    Corrects DMS deploy bug

--- app.py
+++ app.py
@@ -2,11 +2,11 @@
 from aws_cdk import core
 from data_platform.data_lake.stack import DataLakeStack
 from data_platform.data_platform_stack import DataPlatformStack
-from data_platform.dms.stack import DMSStack
+from data_platform.dms.stack import DmsStack
 
 app = core.App()
 data_lake = DataLakeStack(app)
 data_platform = DataPlatformStack(app)
-dms = DMSStack(app, common_stack=DataPlatformStack, 
+dms = DmsStack(app, common_stack=DataPlatformStack, 
                 data_lake_raw_bucket=data_lake.data_lake_raw_bucket)
 app.synth()
